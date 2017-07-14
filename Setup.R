dcf18e1d4a2c8208735c0ad66b679cb2eeb9f121
claudiagonzalez <claudia.gonzalez-leal@minerva.kgi.edu>
2017-07-14 20:21:33 +0200

    Update Setup.R

--- Setup.R
+++ Setup.R
@@ -22,3 +22,16 @@ lumi.raw <- lumiR(fileName,annotationColumn=c("SYMBOL", "PROBE_ID","DEFINITION")
 save(lumi.raw, file="lumi.raw-object")
 write.exprs(lumi.raw,file="lumi.raw_raw data.txt")
 
+## Create function to save images ##
+
+savetiff <- function(fn, dir, density=360, keeppdf=TRUE, ...) {
+  pdffn  <- savepdf(fn, dir, ...)
+  fn     <- paste(fn, ".tiff", sep="")
+  if(!missing(dir))
+    fn <- file.path(dir, fn)
+  cmd   <- paste("convert -density", density, pdffn, "-compress RLE", fn)
+  ## cat(cmd, "\n")
+  system(cmd)
+  if(!keeppdf) file.remove(pdffn)
+  return(fn)
+}
